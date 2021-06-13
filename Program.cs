64cecaa6172348b9827090db96e59de922ec4e37
alexmar000 <82221090+alexmar000@users.noreply.github.com>
2021-06-13 22:25:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pension_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            string userAge = Console.ReadLine();
+            int age = Convert.ToInt32(userAge);
+            int money = 15000;
+
+            do
+            {
+                if (money > 20000 || age < 60)
+                {
+                    Console.WriteLine("You are saving little.");
+                    money = money + 500;
+                }
+                else
+                {
+                    Console.WriteLine("You are saving a lot.");
+                    money = money + 1000;
+                }
+                age++;
+            }
+            while (money < 20000 && age < 65);
+
+            Console.WriteLine("You are retired by the age of " + age + ".");
+            Console.WriteLine("You have saved " + money + " dollars.");
+
+            Console.ReadKey();
+        }
+    }
+}
